f3e06be88706ea201e270e8d47be67022d9e2afd
Buhle Ntushelo <Buhle.Ntushelo@absa.africa>
2022-09-26 15:14:13 +0200

    updating the function

--- printer.py
+++ printer.py
@@ -3,14 +3,7 @@
 def print_header():
       print("Thanks, now I'll print your string reversed, and twice")
 
-def print_reversed(s):
-    return s[::-1]
-
-def print_twice(s):
-    print( s ,s) 
 
 if __name__ == "__main__":
     s = input()
     print_header()
-    print_twice(s)
-    print_reversed(s)
